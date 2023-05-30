199cc03d6a9491cd97f0c0214bd48003e8444214
thomasgsquinaia <thomasquinaia@gmail.com>
2023-05-29 22:30:50 -0300

    up api is better

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package goapitodo
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	Id      string `json: "id"`
+	Name    string `json: "name"`
+	Country string `json: "country"`
+	Age     int64  `json: "age"`
+}
+
+var users = []user{
+	{Id: "1", Name: "Joe", Country: "United States", Age: 25},
+	{Id: "2", Name: "Maria", Country: "Brasil", Age: 22},
+	{Id: "3", Name: "Akemi", Country: "Japan", Age: 45},
+}
+
+func main() {
+	router := gin.Default()
+	router.Run("localhost:9090")
+}
