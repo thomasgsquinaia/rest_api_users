966ec929ea17f07bdc27849401819f2b687062ac
thomasgsquinaia <thomasquinaia@gmail.com>
2023-05-30 22:56:18 -0300

    up insert users

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,7 +20,46 @@ var users = []user{
 	{Id: "3", Name: "Akemi", Country: "Japan", Age: 45},
 }
 
+func getUsers(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, users)
+}
+
+func addUser(context *gin.Context) {
+	var newUser user
+	if err := context.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	users = append(users, newUser)
+	context.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func getUser(context *gin.Context) {
+	id := context.Param("id")
+	user, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, user)
+}
+
+func getTodoById(id string) (*user, error) {
+	for i, u := range users {
+		if u.Id == id {
+			return &users[i], nil
+		}
+	}
+	return nil, errors.New("user not found")
+}
+
 func main() {
 	router := gin.Default()
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUser)
+	router.POST("/users", addUser)
+
 	router.Run("localhost:9090")
 }
